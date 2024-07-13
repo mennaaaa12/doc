0be690d486459a6073de2966546df40f424856d0
menna shady <mennashady7@gmail.com>
2024-07-13 23:13:33 +0300

    =sec

--- newapp/lib/main.dart
+++ newapp/lib/main.dart
@@ -3,7 +3,8 @@ import 'package:newapp/Core/routing/App_route.dart';
 import 'package:newapp/doc_app.dart';
 
 void main() {
-  runApp( DocApp( appRoute: AppRoute()));
   
+  runApp( DocApp( appRoute: AppRoute()));
+
 }
 
