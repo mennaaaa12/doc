0c52d188ebd5f4e870b2e57243b414ee0422faf4
menna shady <mennashady7@gmail.com>
2024-07-25 16:03:14 +0300

    save token secure used sharedpref

--- newapp/lib/Features/login/logic/cubit/login_cubit.dart
+++ newapp/lib/Features/login/logic/cubit/login_cubit.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:newapp/Core/helpers/shared_pref_helpers.dart';
+import 'package:newapp/Core/helpers/constans.dart';
+import 'package:newapp/Core/helpers/shared_prefrence_helper.dart';
 import 'package:newapp/Core/networking/dio_factory.dart';
 import 'package:newapp/Features/login/data/models/login_request_body.dart';
 import 'package:newapp/Features/login/data/repos/login_repo.dart';
 import 'package:newapp/Features/login/logic/cubit/login_state.dart';
-
 
 class LoginCubit extends Cubit<LoginState> {
   final LoginRepo _loginRepo;
@@ -15,21 +15,20 @@ class LoginCubit extends Cubit<LoginState> {
   TextEditingController passwordController = TextEditingController();
   final formKey = GlobalKey<FormState>();
 
+  void emitLoginState() async {
+    emit(const LoginState.loading());
+    final response = await _loginRepo.login(LoginRequestBody(
+        email: emailController.text, password: passwordController.text));
+    response.when(success: (loginResponse) async {
+      await saveUserToken(loginResponse.userData?.token ?? '');
+      emit(LoginState.success(loginResponse));
+    }, failure: (error) {
+      emit(LoginState.failure(message: error.apiErrorModel.message ?? ''));
+    });
+  }
 
-   void emitLoginState()async{
-     emit(const LoginState.loading());
-     final response  =await _loginRepo.login(
-       LoginRequestBody(
-         email: emailController.text,
-         password: passwordController.text
-       )
-     );
-     response.when(success: (loginResponse){
-       emit(LoginState.success(loginResponse));
-     }, failure: (error){
-       emit(LoginState.failure(message: error.apiErrorModel.message??''));
-     });
-   }
-
-
+  Future<void> saveUserToken(String token) async {
+    await SharedPrefHelper.setSecuredString(SharedPrefKeys.userToken, token);
+    DioFactory.setTokenIntoHeaderAfterLogin(token);
+  }
 }
--- newapp/lib/main_production.dart
+++ newapp/lib/main_production.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:newapp/Core/di/dependency_injection.dart';
+import 'package:newapp/Core/helpers/constans.dart';
+import 'package:newapp/Core/helpers/shared_prefrence_helper.dart';
 import 'package:newapp/Core/routing/App_route.dart';
 import 'package:newapp/doc_app.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+   
   setupGetIt();
   await ScreenUtil.ensureScreenSize();
-  runApp( DocApp( appRoute: AppRouter()));
+  await checkedInIfUserIsLoggedIn();
+  runApp(DocApp(appRoute: AppRouter()));
 }
 
+checkedInIfUserIsLoggedIn() async {
+  String userToken = await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken);
+  if (!userToken.isNotEmpty) {
+    isLoggedInUser = true;
+  } else {
+    isLoggedInUser = false;
+  }
+}
